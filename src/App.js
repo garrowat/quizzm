1d852d2887ced0ecb1c9ff3c8f69f83a996de742
Garrett Watson <garrettbauer@gmail.com>
2020-01-16 15:33:27 +0000

    Add buttons for joining lobby and starting a quiz

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
+/* eslint-disable react/jsx-filename-extension */
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import io from 'socket.io-client';
 
 function App() {
   const socket = io('http://localhost:3001');
-  const [challenge, setChallenge] = useState('No challenge');
+  const [name, setName] = useState('Anonymous Coward')
+  const [inLobby, setInLobby] = useState(false);
+  const [quiz, setQuiz] = useState('Start a Quiz');
 
   useEffect(() => {
-    socket.emit('my other event', { my: 'message received' });
+    socket.emit('player connected', { room: 'Someone has connected' });
   });
+
+  const joinLobby = (e) => {
+    e.preventDefault();
+    setInLobby(!inLobby);
+    socket.emit('join lobby', { message: 'Someone has joined the lobby' });
+  };
 
   return (
     <div className="App">
-      Current challenge:
-      { challenge }
+      <input type=""></input>
+      <div>
+        { inLobby ? 'Joined Lobby' : 'Not in Lobby' }
+      </div>
+      <div>
+        <button type="button" onClick={joinLobby}>
+          { inLobby ? 'Leave Lobby' : 'Join lobby' }
+        </button>
+      </div>
+      <div>
+        Current quiz:
+        { quiz }
+      </div>
+      <div>
+        <button>Start Quiz</button>
+      </div>
     </div>
   );
 }
