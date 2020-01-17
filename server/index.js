dbe0f205aa4f0b2612055649a1f53356aab6c060
Garrett Watson <garrettbauer@gmail.com>
2020-01-17 04:13:13 +0000

    Add lobby and quiz joining with data emission

--- server/index.js
+++ server/index.js
@@ -1,29 +1,44 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
 
 const app = express();
 const http = require('http').Server(app);
-
 const io = require('socket.io')(http);
 
+const PORT = process.env.PORT || 3001;
+
+const quizzes = require('./models');
+
+const players = [];
+const lobby = [];
+
 io.on('connection', (socket) => {
+
   socket.on('player connected', (data) => {
-    io.emit('player connected', data);
-    console.log( { data } );
+    players.push(data.newName);
+    io.emit('player connected', players);
+    console.log( { players } );
   });
 
   socket.on('join lobby', (data) => {
-    io.emit('join lobby', data);
-    console.log( socket.id );
+    lobby.push(data.name);
+    io.emit('join lobby', lobby);
+    console.log(data.message);
+  });
+
+  socket.on('quiz started', (data) => {
+    console.log(data.message);
+    io.emit('quiz started', quizzes[0]);
   });
 });
 
-http.listen(3001);
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
