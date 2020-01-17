e67145f9d4f16b7afb84ac52500d6a0d32a0a356
Garrett Watson <garrettbauer@gmail.com>
2020-01-17 04:13:57 +0000

    Complete the skeleton app, showing quizzes to players who are in the lobby

--- src/App.js
+++ src/App.js
@@ -5,37 +5,76 @@ import io from 'socket.io-client';
 
 function App() {
   const socket = io('http://localhost:3001');
-  const [name, setName] = useState('Anonymous Coward')
+  const [name, setName] = useState(null);
   const [inLobby, setInLobby] = useState(false);
-  const [quiz, setQuiz] = useState('Start a Quiz');
+  const [lobbyList, setLobbyList] = useState([]);
+  const [quizStarted, setQuizStarted] = useState(false);
+  const [quiz, setQuiz] = useState(' Start a Quiz');
+  const [playerList, setPlayerList] = useState([]);
 
   useEffect(() => {
-    socket.emit('player connected', { room: 'Someone has connected' });
+    if (name === null) {
+      const newName = `Anonymous_Coward${Math.floor(Math.random() * 1000 + Math.random() * 100)}`;
+      setName(newName);
+      socket.emit('player connected', { newName });
+    }
   });
 
-  const joinLobby = (e) => {
+  useEffect(() => {
+    socket.on('player connected', (payload) => {
+      setPlayerList(payload);
+    });
+
+    socket.on('join lobby', (payload) => {
+      setLobbyList(payload);
+    });
+
+    socket.on('quiz started', (payload) => {
+      setQuiz(payload.question);
+    });
+  });
+
+  const toggleLobby = (e) => {
     e.preventDefault();
     setInLobby(!inLobby);
-    socket.emit('join lobby', { message: 'Someone has joined the lobby' });
+    socket.emit('join lobby', { message: `${name} has joined the lobby`, name });
+  };
+
+  const startQuiz = (e) => {
+    e.preventDefault();
+    socket.emit('quiz started', { message: `${name} has started the quiz` });
+    setQuizStarted(true);
   };
 
   return (
     <div className="App">
-      <input type=""></input>
+      <input type="text" />
       <div>
         { inLobby ? 'Joined Lobby' : 'Not in Lobby' }
       </div>
       <div>
-        <button type="button" onClick={joinLobby}>
+        <button type="button" onClick={toggleLobby}>
           { inLobby ? 'Leave Lobby' : 'Join lobby' }
         </button>
       </div>
       <div>
         Current quiz:
-        { quiz }
+        { inLobby ? quiz : 'Join the lobby to see quizzes' }
       </div>
       <div>
-        <button>Start Quiz</button>
+        <button type="button" onClick={startQuiz}>Start Quiz</button>
+      </div>
+      <div>
+        Players connected:
+        <ul>
+          { playerList.map(player => <li key={player}>{player}</li>) }
+        </ul>
+      </div>
+      <div>
+        Players in lobby:
+        <ul>
+          { lobbyList.map(lobbyPlayer => <li key={`l${lobbyPlayer}`}>{lobbyPlayer}</li>) }
+        </ul>
       </div>
     </div>
   );
