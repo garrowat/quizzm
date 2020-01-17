f687e6f1a5c469e2bb0ee800c91727c073fcc484
Garrett Watson <garrettbauer@gmail.com>
2020-01-17 08:32:07 +0000

    Redo server to focus on the quiz page

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const express = require('express');
 const path = require('path');
 
@@ -6,40 +7,25 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const PORT = process.env.PORT || 3001;
+const router = require('./router');
 
 const quizzes = require('./models');
 
-const players = [];
-const lobby = [];
+const players = {};
+const quiz = {};
 
 io.on('connection', (socket) => {
-
-  socket.on('player connected', (data) => {
-    players.push(data.newName);
-    io.emit('player connected', players);
-    console.log( { players } );
+  socket.on('disconnect', (data) => {
+    console.log('A user disconnected: ', data);
   });
 
-  socket.on('join lobby', (data) => {
-    lobby.push(data.name);
-    io.emit('join lobby', lobby);
-    console.log(data.message);
-  });
-
-  socket.on('quiz started', (data) => {
-    console.log(data.message);
-    io.emit('quiz started', quizzes[0]);
+  socket.on('join room', ({ name, room }) => {
+    console.log({ name, room });
   });
 });
+
+app.use(router);
 
 http.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-app.use(express.static(path.join(__dirname, '../build')));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-
-
