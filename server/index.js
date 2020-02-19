596e3f68585f818dd600f9ece5c4eee0dea600c7
Garrett Watson <garrettbauer@gmail.com>
2020-02-19 23:28:27 +0900

    Allows usage of react environment variables, Add MUI styling and styled components

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+require('dotenv').config();
 
 const {
   addUser,
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,6 +2,25 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
+import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
+const StyledContainer = styled('div')`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledChatContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const StyledTextField = styled(TextField)`
+  margin-top: 5px
+`;
 
 let socket;
 
@@ -10,7 +29,7 @@ const Quiz = ({ location }) => {
   const [room, setRoom] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = 'localhost:3001';
+  const ENDPOINT = `${process.env.REACT_APP_SERVER_IP}:${process.env.REACT_APP_PORT}` || 'localhost:3001';
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -43,19 +62,23 @@ const Quiz = ({ location }) => {
   };
 
   return (
-    <div>
-      <input
-        value={message}
-        onChange={e => setMessage(e.target.value)}
-        onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
-      />
-      <div>
-        {
-          messages.length > 0
-          && messages.map((msg) => <div>`{msg.user}: {msg.text}`</div>)
-        }
-      </div>
-    </div>
+    <StyledContainer>
+      <StyledChatContainer>
+        <p>Chat</p>
+        <StyledTextField
+          variant="outlined"
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          onKeyPress={e => e.key === 'Enter' ? sendMessage(e) : null}
+        />
+        <div>
+          {
+            messages.length > 0
+            && messages.map((msg) => <Typography>{`${msg.user}: ${msg.text}`}</Typography>)
+          }
+        </div>
+      </StyledChatContainer>
+    </StyledContainer>
   );
 };
 
